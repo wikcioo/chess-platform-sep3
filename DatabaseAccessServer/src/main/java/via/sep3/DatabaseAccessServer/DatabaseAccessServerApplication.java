7085299648f0a8316de58ac588e0680942faf914
wiktor <wikcioo@gmail.com>
2022-10-13 09:43:31 +0200

    Initialize database server project.
    Add maven dependencies for spring boot, jpa, postgresql, jackson.

---
+++ DatabaseAccessServer/src/main/java/via/sep3/DatabaseAccessServer/DatabaseAccessServerApplication.java
@@ -0,0 +1,14 @@
+package via.sep3.DatabaseAccessServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.json.JacksonJsonParser;
+
+@SpringBootApplication
+public class DatabaseAccessServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseAccessServerApplication.class, args);
+	}
+
+}
---
+++ DatabaseAccessServer/src/test/java/via/sep3/DatabaseAccessServer/DatabaseAccessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sep3.DatabaseAccessServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseAccessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
