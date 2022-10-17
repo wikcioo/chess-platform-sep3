0889f09b706620764e4f72bbd2589717a726a89b
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-10-18 00:03:03 +0200

    Add login user, post user and get all users endpoints

--- DatabaseAccessServer/src/main/java/via/sep3/DatabaseAccessServer/controller/UserController.java
+++ DatabaseAccessServer/src/main/java/via/sep3/DatabaseAccessServer/controller/UserController.java
@@ -1,10 +1,14 @@
 package via.sep3.DatabaseAccessServer.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import via.sep3.DatabaseAccessServer.domain.User;
 import via.sep3.DatabaseAccessServer.repository.UserRepository;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -13,6 +17,25 @@ public class UserController {
 
     public UserController(UserRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping(path = "/users",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public User Create(@RequestBody User user) {
+        return repository.save(user);
+    }
+
+    @PostMapping(path = "/login",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Boolean Login(@RequestBody User user) throws Exception {
+        User existing = repository.findByEmail(user.getEmail()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"This user does not exist"));
+        return existing.getPassword().equals(user.getPassword());
+    }
+
+    @GetMapping(path = "/users",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<User> GetAll() {
+        return repository.findAll();
     }
 
 }
--- DatabaseAccessServer/src/main/java/via/sep3/DatabaseAccessServer/repository/UserRepository.java
+++ DatabaseAccessServer/src/main/java/via/sep3/DatabaseAccessServer/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package via.sep3.DatabaseAccessServer.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import via.sep3.DatabaseAccessServer.domain.User;
 
 import javax.annotation.Resource;
+import java.util.Optional;
 
 @Repository
 @Resource
 public interface UserRepository extends CrudRepository<User, String> {
-
+    Optional<User> findByEmail(@Param("Email") String email);
 }
