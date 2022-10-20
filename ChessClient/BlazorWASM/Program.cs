b5ba00a717e73f405f6cf1ff78ad7ed6d24d340c
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-10-20 20:29:50 +0200

    Add http client which connect to C# comm server.

--- ChessClient/BlazorWASM/Program.cs
+++ ChessClient/BlazorWASM/Program.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWASM;
+using HttpClients.ClientInterfaces;
+using HttpClients.Implementations;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped(
+    sp =>
+        new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:7233")
+        }
+);
+
+builder.Services.AddScoped<IUserService, UserHttpClient>();
 await builder.Build().RunAsync();
