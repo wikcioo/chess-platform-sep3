6e43a073fb64e32a775d7362add794b836f74dd2
wiktor <wikcioo@gmail.com>
2022-10-24 21:32:56 +0200

    Add UCI compliant stockfish api interface.

---
+++ StockfishServer/StockfishWrapper/IStockfishUci.cs
@@ -0,0 +1,18 @@
+namespace StockfishWrapper;
+
+public enum PositionType { Fen, StartPos }
+
+public interface IStockfishUci
+{
+    void Uci();
+    Task<bool> IsReady();
+    void SetOption(string name, string? value = null);
+    void UciNewGame();
+    void Position(string position, PositionType type);
+    Task<string?> Go(string? searchMoves = null, bool? ponder = null, int? wTime = null, int? bTime = null,
+        int? wInc = null, int? bInc = null, int? movesToGo = null, int? depth = null, int? nodes = null,
+        int? mate = null, int? moveTime = null, bool infinite = false);
+    Task<string?> Stop();
+    void PonderHit();
+    void Quit();
+}
