66a753c12d98abd9881da440805d84b94d7faf9c
selinaceban <selinaceban@gmail.com>
2022-10-25 21:02:35 +0200

    Add Shared Project for authorization and policies.

---
+++ ChessClient/Shared/Auth/AuthorizationPolicies.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Auth;
+
+public static class AuthorizationPolicies
+{
+    public static void AddPolicies(IServiceCollection services)
+    {
+        services.AddAuthorizationCore(options =>
+        
+            options.AddPolicy("MustBeAdmin", a =>
+                a.RequireAuthenticatedUser().RequireClaim("Role", "admin")));
+        }
+    }
+
---
+++ ChessClient/Shared/Dtos/UserLoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Dtos;
+
+public class UserLoginDto
+{
+    public string Username { get; init; }
+    public string Password { get; init; }
+}
