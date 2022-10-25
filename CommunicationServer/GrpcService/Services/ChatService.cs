c8cc3e414e8ba78391ef350548d80eb542706cd8
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-10-25 20:59:06 +0200

    Create chat service which implements grpc methods.

---
+++ CommunicationServer/GrpcService/Services/ChatService.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+
+namespace GrpcService.Services;
+
+public class ChatService : Chat.ChatBase
+{
+    private ChatRoomService _service;
+    private readonly Empty _empty = new();
+
+    public ChatService(ChatRoomService chatRoomService)
+    {
+        _service = chatRoomService;
+    }
+
+    public override async Task StartGame(RequestGame request, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+    {
+        
+        //Add listen here
+        while (!context.CancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                await _service.GetMessagesAsObservable(request)
+                    .ToAsyncEnumerable()
+                    .ForEachAwaitAsync(async (x) => await responseStream.WriteAsync(x), context.CancellationToken)
+                    .ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                break;
+            }
+        }
+    }
+
+
+    public override Task<Empty> Write(Message request, ServerCallContext context)
+    {
+        //Invoke with the new message here
+        Console.WriteLine($"{request.Username}: {request.Body}");
+        _service.Add(request);
+        return Task.FromResult(_empty);
+    }
+}
