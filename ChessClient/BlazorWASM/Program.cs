43a53e05d6fd01f91b0d842713f9ed21439a0580
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-10-25 21:03:46 +0200

    Add grpc web handler.

--- ChessClient/BlazorWASM/Program.cs
+++ ChessClient/BlazorWASM/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWASM;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 using HttpClients.ClientInterfaces;
 using HttpClients.Implementations;
+using Microsoft.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,6 +19,11 @@ builder.Services.AddScoped(
             BaseAddress = new Uri("https://localhost:7233")
         }
 );
+
+builder.Services.AddSingleton(services => GrpcChannel.ForAddress("https://localhost:7289", new GrpcChannelOptions
+{
+    HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+}));
 
 builder.Services.AddScoped<IUserService, UserHttpClient>();
 await builder.Build().RunAsync();
