eaeda332f31d7e4630419000b3b0f0dc647f8bdc
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-10-29 19:30:14 +0200

    Implement grpc game service.

--- CommunicationServer/GrpcService/Program.cs
+++ CommunicationServer/GrpcService/Program.cs
@@ -1,12 +1,17 @@
+using Application.Logic;
+using Application.LogicInterfaces;
 using GrpcService.Services;
+using GrpcService.Services.ChessGame;
+using GrpcService.Services.GameChat;
 using Microsoft.AspNetCore.ResponseCompression;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
 builder.Services.AddResponseCompression(opts =>
 {
     opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
-        new[] {"application/octet-stream"});
+        new[] { "application/octet-stream" });
 });
 builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
 {
@@ -16,18 +21,19 @@ builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
         .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
 }));
 builder.Services.AddSingleton(new ChatRoomService());
+IGameLogic gameLogic = new GameLogic();
+builder.Services.AddSingleton(gameLogic);
 var app = builder.Build();
 app.UseResponseCompression();
 
 
 app.UseStaticFiles();
 
-
-
 app.UseRouting();
 
 app.UseGrpcWeb();
 app.UseCors();
 app.MapGrpcService<ChatService>().EnableGrpcWeb().RequireCors("AllowAll");
+app.MapGrpcService<GameService>().EnableGrpcWeb().RequireCors("AllowAll");
 
 app.Run();
