0c2e3c736dee55038bf58c596fcd8c66b9d7e839
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-01 20:51:16 +0100

    Create a custom authentication provider.

---
+++ ChessClient/BlazorWASM/Auth/CustomAuthProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using HttpClients.ClientInterfaces;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorWASM.Auth;
+
+public class CustomAuthProvider : AuthenticationStateProvider
+{
+    private readonly IAuthService _authService;
+
+    public CustomAuthProvider(IAuthService authService)
+    {
+        _authService = authService;
+        authService.OnAuthStateChanged += AuthStateChanged;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var principal = await _authService.GetAuthAsync();
+        return new AuthenticationState(principal);
+    }
+
+    private void AuthStateChanged(ClaimsPrincipal principal)
+    {
+        NotifyAuthenticationStateChanged(
+            Task.FromResult(
+                new AuthenticationState(principal)
+            )
+        );
+    }
+}
