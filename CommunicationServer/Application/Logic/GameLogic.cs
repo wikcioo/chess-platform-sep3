499c5de7661c675850a906b1ac5896ffa37ffd49
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-04 00:06:19 +0100

    Set player names in game logic.

--- CommunicationServer/Application/Logic/GameLogic.cs
+++ CommunicationServer/Application/Logic/GameLogic.cs
@@ -11,38 +11,43 @@ public class GameLogic : IGameLogic
     private ulong _nextGameId;
     private readonly Dictionary<ulong, GameRoom> _gameRooms = new();
 
-    public Task<ResponseGameDto> StartGame(RequestGameDto dto){
-        ResponseGameDto responseDto = new(){
-            Success=true,
+    public Task<ResponseGameDto> StartGame(RequestGameDto dto)
+    {
+        ResponseGameDto responseDto = new()
+        {
+            Success = true,
             GameRoom = _nextGameId,
             Opponent = dto.Opponent ?? "StockfishAI",
             Fen = Fen.StartPositionFen,
-            IsWhite = dto.IsWhite??true
+            IsWhite = dto.IsWhite ?? true
         };
-        
-        var gameType = dto.GameType switch {
+
+        var gameType = dto.GameType switch
+        {
             "AI" => GameStateTypes.Ai,
             "Friend" => GameStateTypes.Friend,
             "Random" => GameStateTypes.Random,
-            _ => throw new Exception("Invalid Game type exception") 
+            _ => throw new Exception("Invalid Game type exception")
         };
 
         GameRoom gameRoom = new(gameType, dto.Seconds, dto.Increment);
+        gameRoom.PlayerWhite = responseDto.IsWhite ? dto.Username : dto.Opponent;
+        gameRoom.PlayerBlack = responseDto.IsWhite ? dto.Opponent : dto.Username;
+
         _gameRooms.Add(_nextGameId++, gameRoom);
-        
+
         return Task.FromResult(responseDto);
     }
 
-    public IObservable<JoinedGameStreamDto> JoinGame(RequestJoinGameDto dto){
-        
-        if(_gameRooms.ContainsKey(dto.GameRoom))
+    public IObservable<JoinedGameStreamDto> JoinGame(RequestJoinGameDto dto)
+    {
+        if (_gameRooms.ContainsKey(dto.GameRoom))
         {
             GameRoom gameRoom = _gameRooms[dto.GameRoom];
             return gameRoom.GetMovesAsObservable();
         }
-        
-        throw new Exception("Game not found");
 
+        throw new Exception("Game not found");
     }
 
     public Task<AckTypes> MakeMove(MakeMoveDto dto)
