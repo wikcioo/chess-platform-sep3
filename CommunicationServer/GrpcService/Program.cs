4eb1ee4e00742f1e2afaaf97f670f63b1f31511e
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-04 00:05:02 +0100

    Add Jwt authentication to grpc service.

--- CommunicationServer/GrpcService/Program.cs
+++ CommunicationServer/GrpcService/Program.cs
@@ -1,9 +1,13 @@
+using System.Text;
 using Application.Logic;
 using Application.LogicInterfaces;
+using Domain.Auth;
 using GrpcService.Services;
 using GrpcService.Services.ChessGame;
 using GrpcService.Services.GameChat;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,28 +15,45 @@ builder.Services.AddGrpc();
 builder.Services.AddResponseCompression(opts =>
 {
     opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
-        new[] { "application/octet-stream" });
+        new[] {"application/octet-stream"});
 });
 builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
 {
     builder.AllowAnyOrigin()
         .AllowAnyMethod()
         .AllowAnyHeader()
-        .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+        .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding", "Authorization");
 }));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+builder.Services.AddAuthorization();
+AuthorizationPolicies.AddPolicies(builder.Services);
 builder.Services.AddSingleton(new ChatRoomService());
 IGameLogic gameLogic = new GameLogic();
 builder.Services.AddSingleton(gameLogic);
+
 var app = builder.Build();
 app.UseResponseCompression();
-
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseGrpcWeb();
 app.UseCors();
+
 app.MapGrpcService<ChatService>().EnableGrpcWeb().RequireCors("AllowAll");
 app.MapGrpcService<GameService>().EnableGrpcWeb().RequireCors("AllowAll");
 
