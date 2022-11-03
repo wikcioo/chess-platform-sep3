fb10ff172209c7687de96b8ad91aed43c82fdf51
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-04 00:12:50 +0100

    Add a jwt token getter method to interface.

--- ChessClient/HttpClients/ClientInterfaces/IAuthService.cs
+++ ChessClient/HttpClients/ClientInterfaces/IAuthService.cs
@@ -7,8 +7,9 @@ public interface IAuthService
 {
     public Task LoginAsync(string email, string password);
     public Task LogoutAsync();
-    public Task RegisterAsync(User user);
     public Task<ClaimsPrincipal> GetAuthAsync();
 
     public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
+
+    public string GetJwtToken();
 }
