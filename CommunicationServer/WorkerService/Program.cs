62d29ff1ad8cf603a2c570ffd50778436ab01931
M0Rek <morek.090@gmail.com>
2022-11-04 18:27:43 +0100

    Remove worker service.

--- CommunicationServer/WorkerService/Program.cs
+++
@@ -1,7 +0,0 @@
-using WorkerService;
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
-    .Build();
-
-await host.RunAsync();
