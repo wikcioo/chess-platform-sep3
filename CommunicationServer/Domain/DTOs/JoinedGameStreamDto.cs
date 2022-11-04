a756bd2c0a188b60c222b8ffff2dfceff6a3cd32
wiktor <wikcioo@gmail.com>
2022-11-04 08:26:28 +0100

    Add event element and initialize string fields to empty strings.

--- CommunicationServer/Domain/DTOs/JoinedGameStreamDto.cs
+++ CommunicationServer/Domain/DTOs/JoinedGameStreamDto.cs
@@ -2,8 +2,9 @@ namespace Domain.DTOs;
 
 public class JoinedGameStreamDto
 {
-    public string FenString { get; set; }
+    public string FenString { get; set; } = string.Empty;
     public uint GameEndType { get; set; }
     public double TimeLeftMs { get; set; }
     public bool IsWhite { get; set; }
+    public string Event { get; set; } = string.Empty;
 }
