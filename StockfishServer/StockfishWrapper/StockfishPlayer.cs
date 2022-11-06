ba9c06384afd55d78d70c84859952627fe6fc955
M0Rek <morek.090@gmail.com>
2022-11-06 19:12:52 +0100

    Add stockfish player and levels to model.

---
+++ StockfishServer/StockfishWrapper/StockfishPlayer.cs
@@ -0,0 +1,15 @@
+namespace StockfishWrapper;
+
+public class StockfishPlayer
+{
+   public uint Skill { get; private set; }
+   public uint Depth { get; private set; }
+   public uint Time { get; private set; }
+
+   public StockfishPlayer(uint skill, uint depth, uint time)
+   {
+      Skill = skill;
+      Depth = depth;
+      Time = time;
+   }
+}
