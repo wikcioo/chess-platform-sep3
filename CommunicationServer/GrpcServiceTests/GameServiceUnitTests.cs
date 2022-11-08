233115db20d67a627ef94840e4603ff6c4e1a6af
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-08 14:44:07 +0100

    Add test for offering and accepting draw as the same person.

--- CommunicationServer/GrpcServiceTests/GameServiceUnitTests.cs
+++ CommunicationServer/GrpcServiceTests/GameServiceUnitTests.cs
@@ -127,6 +127,27 @@ public class GameServiceUnitTests
         }, serverCallContext);
         Assert.Equal((uint) AckTypes.NotUserTurn, response.Status);
     }
+    
+    [Fact]
+    public async Task OfferingADrawWithValidUserAndAcceptingItWithTheSameUser()
+    {
+        var responseGame = await _gameService.StartGame(_requestGame, TestServerCallContext.Create());
+        var serverCallContext = SetUpAuthenticated("Jeff");
+        await _gameService.OfferDraw(new RequestDraw
+        {
+            GameRoom = responseGame.GameRoom,
+            Username = "Jeff"
+        }, serverCallContext);
+        
+        var serverCallContext2 = SetUpAuthenticated("Jeff");
+        var drawOfferResponse =  await _gameService.DrawOfferResponse(new ResponseDraw
+        {
+            Accept = true,
+            GameRoom = responseGame.GameRoom,
+            Username = "Alice"
+        },serverCallContext2);
+        Assert.Equal((uint) AckTypes.NotUserTurn, drawOfferResponse.Status);
+    }
     [Fact]
     public async Task OfferingADrawWithValidUserAndAcceptingItWithValidUserReturnsSuccess()
     {
