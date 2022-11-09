072a7f4677841b2e23a8b78ef8d64f7d20c16ab2
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-09 14:58:25 +0100

    Use message dto in chat room and move it to application layer.

---
+++ CommunicationServer/Application/Entities/ChatRoom.cs
@@ -0,0 +1,35 @@
+﻿using System.Reactive.Linq;
+using Domain.DTOs.Chat;
+
+namespace Application.Entities;
+
+public class ChatRoom
+{
+    private List<string> _users;
+
+    public event Action<MessageDto> MessageReceived;
+    private List<MessageDto> _logs = new();
+
+    public ChatRoom(string user1, string user2)
+    {
+        _users = new List<string>
+        {
+            user1,
+            user2
+        };
+    }
+
+    public void Add(MessageDto message)
+    {
+        _logs.Add(message);
+        MessageReceived?.Invoke(message);
+    }
+
+    public IObservable<MessageDto> GetMessagedAsObservable()
+    {
+        var newLogs = Observable.FromEvent<MessageDto>(
+            (x) => MessageReceived += x,
+            (x) => MessageReceived -= x);
+        return _logs.ToObservable().Concat(newLogs);
+    }
+}
