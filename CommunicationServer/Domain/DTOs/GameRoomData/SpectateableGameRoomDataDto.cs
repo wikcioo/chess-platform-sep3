0b617e1e8d915f824eb28b8b4c79e5755998fb71
wikcioo <wikcioo@gmail.com>
2022-11-13 16:23:10 +0100

    Add spectateable game room data dto.

---
+++ CommunicationServer/Domain/DTOs/GameRoomData/SpectateableGameRoomDataDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.DTOs.GameRoomData;
+
+public class SpectateableGameRoomDataDto
+{
+    public ulong GameRoom { get; set; }
+    public string UsernameWhite { get; set; } = string.Empty;
+    public string UsernameBlack { get; set; } = string.Empty;
+    public uint Seconds { get; set; }
+    public uint Increment { get; set; }
+}
