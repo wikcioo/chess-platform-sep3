0f006480caef0214f12c00497856a88c8ddabe04
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-25 12:40:14 +0100

    Authenticate admin role on create user endpoint.

--- ChessClient/HttpClients/Implementations/UserHttpClient.cs
+++ ChessClient/HttpClients/Implementations/UserHttpClient.cs
@@ -2,20 +2,25 @@
 using System.Text.Json;
 using Domain.Models;
 using HttpClients.ClientInterfaces;
+using System.Net.Http.Headers;
 
 namespace HttpClients.Implementations;
 
 public class UserHttpClient : IUserService
 {
     private readonly HttpClient _client;
+    private readonly IAuthService _authService;
 
-    public UserHttpClient(HttpClient client)
+    public UserHttpClient(HttpClient client, IAuthService authService)
     {
         _client = client;
+        _authService = authService;
     }
 
     public async Task<User> CreateAsync(User user)
     {
+        _client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", _authService.GetJwtToken());
         var response = await _client.PostAsJsonAsync("/users", user);
         var result = await response.Content.ReadAsStringAsync();
         if (!response.IsSuccessStatusCode)
