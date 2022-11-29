2f6f09fefaaff9e1a939b94a910ec261f43e5957
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-11-29 13:26:59 +0100

    Remove grpc project and grpc tests.

--- CommunicationServer/GrpcService/Hubs/IGameHub.cs
+++
@@ -1,9 +0,0 @@
-using Domain.DTOs.Chat;
-
-namespace GrpcService.Hubs;
-
-public interface IGameHub
-{
-    Task ReceiveMessage(string username, string body);
-    Task GetLog(List<MessageDto> log);
-}
