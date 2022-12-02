4c34579aad8afee7fbceece2a0302d602c0a4eea
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-12-02 19:33:57 +0100

    Remove stockfish service from client.

--- ChessClient/HttpClients/ClientInterfaces/IStockfishService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.DTOs.Stockfish;
-
-namespace HttpClients.ClientInterfaces;
-
-public interface IStockfishService
-{
-    Task<bool> GetStockfishIsReadyAsync();
-    Task<string> GetBestMoveAsync(StockfishBestMoveDto dto);
-}
