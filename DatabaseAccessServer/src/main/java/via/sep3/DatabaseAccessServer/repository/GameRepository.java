309b5195a775024b3f10ab4cdac0b80b5ba152d0
selinaceban <selinaceban@gmail.com>
2022-12-06 16:48:16 +0100

    Fix imports in GameRepository.java.

--- DatabaseAccessServer/src/main/java/via/sep3/DatabaseAccessServer/repository/GameRepository.java
+++ DatabaseAccessServer/src/main/java/via/sep3/DatabaseAccessServer/repository/GameRepository.java
@@ -2,13 +2,11 @@ package via.sep3.DatabaseAccessServer.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import via.sep3.DatabaseAccessServer.domain.Game;
-import via.sep3.DatabaseAccessServer.domain.User;
 
 import java.util.Optional;
 
 public interface GameRepository extends CrudRepository<Game, String> {
-Optional<Game> findByCreatorIgnoreCase(@Param("creator") String creator);
-    Optional<Game> findByGameId(@Param("gameId") int gameId);
+Iterable<Game> findByCreatorIgnoreCase(@Param("creator") String creator);
+Optional<Game> findByGameId(@Param("gameId") int gameId);
 }
