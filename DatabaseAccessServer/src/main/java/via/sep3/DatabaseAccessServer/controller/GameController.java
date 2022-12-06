a2019cbf1b2f7f3bb16e27132a47cf4b1291df42
selinaceban <selinaceban@gmail.com>
2022-12-06 21:03:20 +0100

    Update Game Creation method.

--- DatabaseAccessServer/src/main/java/via/sep3/DatabaseAccessServer/controller/GameController.java
+++ DatabaseAccessServer/src/main/java/via/sep3/DatabaseAccessServer/controller/GameController.java
@@ -1,37 +1,64 @@
 package via.sep3.DatabaseAccessServer.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import via.sep3.DatabaseAccessServer.domain.DTOs.GameCreationDto;
 import via.sep3.DatabaseAccessServer.domain.Game;
+import via.sep3.DatabaseAccessServer.domain.User;
+import via.sep3.DatabaseAccessServer.domain.enums.GameType;
 import via.sep3.DatabaseAccessServer.repository.GameRepository;
+import via.sep3.DatabaseAccessServer.repository.UserRepository;
 
 import javax.annotation.Resource;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 
 public class GameController {
     @Resource
-    private final GameRepository repository;
+    private final GameRepository gameRepository;
 
-    public GameController(GameRepository repository) {
-        this.repository = repository;
+    @Resource
+    private final UserRepository userRepository;
+
+
+    public GameController(GameRepository gameRepository, UserRepository userRepository) {
+        this.gameRepository = gameRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    @PostMapping(path = "/games",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Game Create(@RequestBody GameCreationDto dto) {
+        User creator = userRepository.findByEmailIgnoreCase(dto.getCreator()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Creator does not exist."));
+        User playerBlack = userRepository.findByEmailIgnoreCase(dto.getPlayerBlack()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Player Black does not exist."));
+        User playerWhite = userRepository.findByEmailIgnoreCase(dto.getPlayerWhite()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Player White does not exist."));
+        Game game = new Game(creator, dto.getGameType(), playerWhite, playerBlack, dto.getTimeControlDurationSeconds(), dto.getTimeControlIncrementSeconds());
+        return gameRepository.save(game);
     }
 
     @GetMapping(path = "/games",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<Game> getAll(@RequestParam Map<String, String> allRequestParams) {
+    public Iterable<Game> GetAll(@RequestParam Map<String, String> allRequestParams) {
         Iterable<Game> games;
-
-        if(!allRequestParams.containsKey("gameId")) {
-            games = repository.findAll();
-        }
-        else {
-            games = repository.findByCreatorIgnoreCase(allRequestParams.get("creator"));
+        if (!allRequestParams.containsKey("gameId")) {
+            games = gameRepository.findAll();
+        } else {
+            games = gameRepository.findByCreatorEquals(allRequestParams.get("creator"));
         }
         return games;
+    }
+
+    @GetMapping(path = "/games/{gameId}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Optional<Game> GetByGameId(@PathVariable("gameId") int gameId) {
+        Optional<Game> game;
+        game = gameRepository.findByGameId(gameId);
+        return game;
     }
 
 
