6041da20ad22bd276e8df7edd5e0eea4b17badf3
selinaceban <selinaceban@gmail.com>
2022-12-07 16:01:03 +0100

    Create GameCreationDto.

---
+++ CommunicationServer/Domain/DTOs/GameCreationDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enums;
+
+namespace Domain.DTOs;
+
+public class GameCreationDto
+{
+    public string Creator { get; set; }
+    public string? PlayerWhite { get; set; }
+    public string? PlayerBlack { get; set; }
+    public OpponentTypes GameType { get; set; }
+    public  GameOutcome GameOutcome { get; set; }
+
+    public bool IsVisible { get; set; }
+
+    public uint TimeControlDurationSeconds { get; set; }
+    public uint TimeControlIncrementSeconds { get; set; }
+}
