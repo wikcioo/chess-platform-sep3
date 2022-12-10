5836fe6996063b1d63f08e30b4ef61a29a99df87
Wiktor Ciolkowski <wikcioo@gmail.com>
2022-12-10 19:44:44 +0100

    Clean-up BlazorWASM project and fix inconsistencies.

--- ChessClient/BlazorWASM/Program.cs
+++ ChessClient/BlazorWASM/Program.cs
@@ -15,7 +15,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 builder.Services.AddScoped(
     _ =>
         new HttpClient
