f9c6ae558050bb7d76071673862fce628df2cd76
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-12-14 19:24:25 +0100

    Uncomment game logic unit tests.

--- CommunicationServer/Application.Tests/GameLogicUnitTests.cs
+++ CommunicationServer/Application.Tests/GameLogicUnitTests.cs
@@ -1,87 +1,154 @@
-// using System.Collections.Generic;
-// using System.Net.Http;
-// using Application.Logic;
-// using Application.LogicInterfaces;
-// using DatabaseClient.Implementations;
-// using Domain.DTOs;
-// using Domain.Enums;
-// using Rudzoft.ChessLib.Fen;
-// using Xunit;
-//
-// namespace Application.Tests;
-//
-// public class GameLogicUnitTests
-// {
-//     public IGameLogic GameLogic = new GameLogic(new StockfishHttpClient(new HttpClient()),new ChatLogic());
-//
-//     [Theory]
-//     [InlineData("Alice")]
-//     [InlineData("StockfishAi1")]
-//     [InlineData(null)]
-//     public void StartingGameReturnsCorrectResponseDto(string? opponent)
-//     {
-//         var response = GameLogic.StartGame(new RequestGameDto()
-//         {
-//             Username = "Jeff",
-//             OpponentType = OpponentTypes.Friend,
-//             IncrementSeconds = 0,
-//             Side = GameSides.White,
-//             OpponentName = opponent,
-//             DurationSeconds = 600
-//         });
-//
-//         var expectedResponse = new ResponseGameDto()
-//         {
-//             Success = true,
-//             IsWhite = true,
-//             Opponent = opponent ?? "StockfishAI01",
-//             Fen = Fen.StartPositionFen
-//         };
-//
-//         Assert.Equal(expectedResponse.Success, response.Result.Success);
-//         Assert.Equal(expectedResponse.Opponent, response.Result.Opponent);
-//         Assert.Equal(expectedResponse.IsWhite, response.Result.IsWhite);
-//         Assert.Equal(expectedResponse.Fen, response.Result.Fen);
-//     }
-//
-//     [Fact]
-//     public void JoinRoomThrowsArgumentExceptionWhenNoRoomFound()
-//     {
-//         Assert.Throws<KeyNotFoundException>(() =>
-//         {
-//             GameLogic.JoinGame(new RequestJoinGameDto()
-//             {
-//                 Username = "Jeff",
-//                 GameRoom = 0
-//             });
-//         });
-//     }
-//
-//     [Fact]
-//     public void MakeMoveReturnsGameNotFoundWhenNoRoomFound()
-//     {
-//         var ack = GameLogic.MakeMove(new MakeMoveDto());
-//         Assert.True(ack.Result == AckTypes.GameNotFound);
-//     }
-//
-//     [Fact]
-//     public void ResignReturnsGameNotFoundWhenNoRoomFound()
-//     {
-//         var ack = GameLogic.Resign(new RequestResignDto());
-//         Assert.True(ack.Result == AckTypes.GameNotFound);
-//     }
-//
-//     [Fact]
-//     public void OfferDrawReturnsGameNotFoundWhenNoRoomFound()
-//     {
-//         var ack = GameLogic.OfferDraw(new RequestDrawDto());
-//         Assert.True(ack.Result == AckTypes.GameNotFound);
-//     }
-//
-//     [Fact]
-//     public void DrawOfferResponseReturnsGameNotFoundWhenNoRoomFound()
-//     {
-//         var ack = GameLogic.DrawOfferResponse(new ResponseDrawDto());
-//         Assert.True(ack.Result == AckTypes.GameNotFound);
-//     }
-// }
+using System.Collections.Generic;
+using System.Net.Http;
+using Application.Logic;
+using Application.LogicInterfaces;
+using Domain.Enums;
+using Rudzoft.ChessLib.Fen;
+
+namespace Application.Tests;
+
+using ClientInterfaces;
+using Domain.DTOs.Draw;
+using Domain.DTOs.Game;
+using Domain.DTOs.Resignation;
+using Domain.DTOs.StartGame;
+using Domain.DTOs.Stockfish;
+using Domain.Models;
+using GameRoomHandlers;
+using Moq;
+
+
+public class GameLogicUnitTests
+{
+
+
+    private readonly IGameLogic _gameLogic;
+    private readonly Mock<IUserService> _userServiceMock;
+
+    public GameLogicUnitTests()
+    {
+        var stockfishServiceMock = new Mock<IStockfishService>();
+        stockfishServiceMock.Setup(p => p.GetBestMoveAsync(It.IsAny<StockfishBestMoveDto>())).ReturnsAsync("e2e4");
+        _userServiceMock = new Mock<IUserService>();
+        _userServiceMock.Setup(p => p.GetByUsernameAsync(It.IsAny<string>())).ReturnsAsync(new User());
+        var gameServiceMock = new Mock<IGameService>();
+        _gameLogic = new GameLogic(stockfishServiceMock.Object, new ChatLogic(), _userServiceMock.Object, gameServiceMock.Object, new GameRoomHandlerFactory());
+    }
+
+    [Theory]
+    [InlineData("Alice", OpponentTypes.Friend)]
+    [InlineData("StockfishAi1", OpponentTypes.Ai)]
+    public void StartingGameReturnsCorrectResponseDto(string? opponent, OpponentTypes opponentType)
+    {
+        var response = _gameLogic.StartGame(new RequestGameDto()
+        {
+            Username = "Jeff",
+            OpponentType = opponentType,
+            IncrementSeconds = 0,
+            Side = GameSides.Black,
+            OpponentName = opponent,
+            DurationSeconds = 600
+        });
+
+        Assert.True(response.Result.Success);
+        Assert.Equal(opponent, response.Result.Opponent);
+        Assert.Equal(Fen.StartPositionFen, response.Result.Fen);
+    }
+
+
+    [Theory]
+    [InlineData("Alice", OpponentTypes.Friend)]
+    [InlineData("Bob", OpponentTypes.Friend)]
+    public void StartingGameReturnsFalseWhenUserNotFound(string? opponent, OpponentTypes opponentType)
+    {
+        _userServiceMock.Setup(p => p.GetByUsernameAsync(It.IsAny<string>())).ReturnsAsync((User?)null);
+        var response = _gameLogic.StartGame(new RequestGameDto()
+        {
+            Username = "Jeff",
+            OpponentType = opponentType,
+            IncrementSeconds = 0,
+            Side = GameSides.Black,
+            OpponentName = opponent,
+            DurationSeconds = 600
+        });
+
+        Assert.False(response.Result.Success);
+    }
+
+    [Theory]
+    [InlineData("Alice", OpponentTypes.Random)]
+    [InlineData("StockfishAi1", OpponentTypes.Random)]
+    public void StartingGameAgainstRandomWithOpponentSetFails(string opponent, OpponentTypes opponentType)
+    {
+        var response = _gameLogic.StartGame(new RequestGameDto()
+        {
+            Username = "Jeff",
+            OpponentType = opponentType,
+            IncrementSeconds = 0,
+            Side = GameSides.Black,
+            OpponentName = opponent,
+            DurationSeconds = 600
+        });
+
+        Assert.False(response.Result.Success);
+        Assert.Equal("Opponent cannot be chosen for a random game.", response.Result.ErrorMessage);
+    }
+
+    [Theory]
+    [InlineData("StockfishAi1", OpponentTypes.Friend)]
+    public void StartingGameAgainstFriendWithStockfishOpponentFails(string opponent, OpponentTypes opponentType)
+    {
+        var response = _gameLogic.StartGame(new RequestGameDto()
+        {
+            Username = "Jeff",
+            OpponentType = opponentType,
+            IncrementSeconds = 0,
+            Side = GameSides.Black,
+            OpponentName = opponent,
+            DurationSeconds = 600
+        });
+        Assert.False(response.Result.Success);
+        Assert.Equal("Opponent is an AI in the not ai game.", response.Result.ErrorMessage);
+    }
+
+    [Fact]
+    public void JoinRoomThrowsArgumentExceptionWhenNoRoomFound()
+    {
+        Assert.Throws<KeyNotFoundException>(() =>
+        {
+            _gameLogic.JoinGame(new RequestJoinGameDto()
+            {
+                Username = "Jeff",
+                GameRoom = 0
+            });
+        });
+    }
+
+    [Fact]
+    public void MakeMoveReturnsGameNotFoundWhenNoRoomFound()
+    {
+        var ack = _gameLogic.MakeMove(new MakeMoveDto());
+        Assert.True(ack.Result == AckTypes.GameNotFound);
+    }
+
+    [Fact]
+    public void ResignReturnsGameNotFoundWhenNoRoomFound()
+    {
+        var ack = _gameLogic.Resign(new RequestResignDto());
+        Assert.True(ack.Result == AckTypes.GameNotFound);
+    }
+
+    [Fact]
+    public void OfferDrawReturnsGameNotFoundWhenNoRoomFound()
+    {
+        var ack = _gameLogic.OfferDraw(new RequestDrawDto());
+        Assert.True(ack.Result == AckTypes.GameNotFound);
+    }
+
+    [Fact]
+    public void DrawOfferResponseReturnsGameNotFoundWhenNoRoomFound()
+    {
+        var ack = _gameLogic.DrawOfferResponse(new ResponseDrawDto());
+        Assert.True(ack.Result == AckTypes.GameNotFound);
+    }
+}
