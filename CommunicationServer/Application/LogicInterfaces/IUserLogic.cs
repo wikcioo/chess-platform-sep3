32ce92b2415d346a51c09e8a5267d5a56659815a
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-12-14 21:53:21 +0100

    Remove login from user controller and logic.

--- CommunicationServer/Application/LogicInterfaces/IUserLogic.cs
+++ CommunicationServer/Application/LogicInterfaces/IUserLogic.cs
@@ -5,7 +5,6 @@ namespace Application.LogicInterfaces;
 
 public interface IUserLogic
 {
-    Task<User> LoginAsync(UserLoginDto dto);
     Task<User> CreateAsync(User user);
 
     Task<IEnumerable<UserSearchResultDto>> GetInsensitiveAsync(UserSearchParamsDto paramsDto);
